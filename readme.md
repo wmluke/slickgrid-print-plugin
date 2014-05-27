bcbc0d0a6445a7c2e0f144dcde56efa6fbe09e8f
wmluke <wmlukeb@gmail.com>
2014-05-27 01:08:00 -0700

    Added readme

---
+++ readme.md
@@ -0,0 +1,22 @@
+# slickgrid-print-plugin
+
+Create a printable view of a [SlickGird](https://github.com/mleibman/SlickGrid) as a simple a HTML table.
+
+Note: this plugin is still pre-alpha.
+
+## Usage 
+```javascript
+var printPlugin = new Slick.Plugins.Print();
+
+grid = new Slick.Grid("#grid", data, columns, options);
+grid.registerPlugin(printPlugin);
+
+$('.btn-print').on('click', function () {
+    var strWindowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+    printPlugin.printToWindow(window.open('/print-grid.html', 'print_window', strWindowFeatures));
+});
+
+```
+
+## License
+MIT
--- test/app/scripts/app.js
+++ test/app/scripts/app.js
@@ -3,12 +3,12 @@
 
     var grid;
     var columns = [
-        {id: "title", name: "Title", field: "title"},
-        {id: "duration", name: "Duration", field: "duration"},
-        {id: "%", name: "% Complete", field: "percentComplete"},
-        {id: "start", name: "Start", field: "start"},
-        {id: "finish", name: "Finish", field: "finish"},
-        {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
+        {id: 'title', name: 'Title', field: 'title'},
+        {id: 'duration', name: 'Duration', field: 'duration'},
+        {id: '%', name: '% Complete', field: 'percentComplete'},
+        {id: 'start', name: 'Start', field: 'start'},
+        {id: 'finish', name: 'Finish', field: 'finish'},
+        {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven'}
     ];
 
     var options = {
@@ -20,24 +20,24 @@
         var data = [];
         for (var i = 0; i < 500; i++) {
             data[i] = {
-                title: "Task " + i,
-                duration: "5 days",
+                title: 'Task ' + i,
+                duration: '5 days',
                 percentComplete: Math.round(Math.random() * 100),
-                start: "01/01/2009",
-                finish: "01/05/2009",
-                effortDriven: (i % 5 == 0)
+                start: '01/01/2009',
+                finish: '01/05/2009',
+                effortDriven: (i % 5 === 0)
             };
         }
 
         var printPlugin = new Slick.Plugins.Print();
 
-        grid = new Slick.Grid("#grid", data, columns, options);
+        grid = new Slick.Grid('#grid', data, columns, options);
         grid.registerPlugin(printPlugin);
 
         $('.btn-print').on('click', function () {
-            var strWindowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+            var strWindowFeatures = 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes';
             printPlugin.printToWindow(window.open('/print-grid.html', 'print_window', strWindowFeatures));
         });
-    })
+    });
 
 }());
