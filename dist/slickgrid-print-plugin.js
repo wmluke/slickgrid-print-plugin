193ff6029c01ba758fd1fa8570db54a489f208f9
wmluke <wmlukeb@gmail.com>
2014-05-27 00:14:59 -0700

    Created simple printing support

--- dist/slickgrid-print-plugin.js
+++ dist/slickgrid-print-plugin.js
@@ -3,6 +3,85 @@
  * Copyright (c) 2013 . 
  * License: MIT
  */
-(function () {
+(function ($) {
     'use strict';
-}());
+
+    var SlickPrint = function () {
+
+        var _self = this;
+        var _grid;
+
+        this.init = function (grid) {
+            _grid = grid;
+        };
+
+        this.printToHtml = function () {
+            var numRows = _grid.getDataLength();
+            var columns = _grid.getColumns();
+            var numCols = columns.length;
+            var r, c;
+            var rows = [], cols = [], headers = [];
+            var cellNode;
+            var topRow = _grid.getRenderedRange().top;
+
+            columns.forEach(function (col) {
+                headers.push(col.name);
+            });
+
+            Slick.GlobalEditorLock.cancelCurrentEdit();
+
+            _grid.scrollRowToTop(0);
+
+            for (r = 0; r < numRows; r++) {
+                cols = [];
+                for (c = 0; c < numCols; c++) {
+                    cellNode = _grid.getCellNode(r, c);
+                    if (!cellNode) {
+                        _grid.scrollRowToTop(r);
+                        cellNode = _grid.getCellNode(r, c);
+                    }
+                    cols.push($(cellNode).text());
+                }
+                rows.push('<td>' + cols.join('</td><td>') + '</td>');
+            }
+
+            var table = [
+                '<table class="table table-bordered">',
+                '<thead>',
+                '<tr>',
+                    '<th>' + headers.join('</th><th>') + '</th>',
+                '</tr>',
+                '</thead>',
+                '<tbody>',
+                    '<tr>' + rows.join('</tr>\n<tr>') + '</tr>',
+                '</tbody>',
+                '</table>'
+            ].join('\n');
+
+            _grid.scrollRowToTop(topRow);
+
+            return table;
+        };
+
+        this.printToElement = function ($element) {
+            $($element).html(_self.printToHtml());
+        };
+
+        this.printToWindow = function (w) {
+            w.onload = function () {
+                setTimeout(function () {
+                    _self.printToElement(w.document.body);
+                });
+            };
+        };
+    };
+
+    // register namespace
+    $.extend(true, window, {
+        "Slick": {
+            "Plugins": {
+                "Print": SlickPrint
+            }
+        }
+    });
+}(jQuery));
--- test/app/scripts/app.js
+++ test/app/scripts/app.js
@@ -1,4 +1,43 @@
 (function () {
     'use strict';
 
+    var grid;
+    var columns = [
+        {id: "title", name: "Title", field: "title"},
+        {id: "duration", name: "Duration", field: "duration"},
+        {id: "%", name: "% Complete", field: "percentComplete"},
+        {id: "start", name: "Start", field: "start"},
+        {id: "finish", name: "Finish", field: "finish"},
+        {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
+    ];
+
+    var options = {
+        enableCellNavigation: true,
+        enableColumnReorder: false
+    };
+
+    $(function () {
+        var data = [];
+        for (var i = 0; i < 500; i++) {
+            data[i] = {
+                title: "Task " + i,
+                duration: "5 days",
+                percentComplete: Math.round(Math.random() * 100),
+                start: "01/01/2009",
+                finish: "01/05/2009",
+                effortDriven: (i % 5 == 0)
+            };
+        }
+
+        var printPlugin = new Slick.Plugins.Print();
+
+        grid = new Slick.Grid("#grid", data, columns, options);
+        grid.registerPlugin(printPlugin);
+
+        $('.btn-print').on('click', function () {
+            var strWindowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+            printPlugin.printToWindow(window.open('/print-grid.html', 'print_window', strWindowFeatures));
+        });
+    })
+
 }());
